124bc6730b1614df67ccf8ea45955571e0e5c188
Ngowda94 <Nagraj94>
2022-06-30 21:11:17 +0530

    basic to print message

---
+++ src/com/bridgelabz/basics/PrintMessage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.basics;
+
+public class PrintMessage {
+    public static void main(String[] args) {
+        System.out.print("Hello World! ");
+        System.out.print("i'm beginner to java.");
+    }
+}
